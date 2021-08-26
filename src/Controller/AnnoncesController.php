a57834c28f3f08a5f9baac12d2ea64edcbf73779
Bastien PIPEREL <bastienpiperel@yahoo.fr>
2021-08-26 13:50:46 +0200

    ajout du formulaire de contact

--- src/Controller/AnnoncesController.php
+++ src/Controller/AnnoncesController.php
@@ -3,24 +3,43 @@
 namespace App\Controller;
 
 use App\Entity\Annonces;
+use App\Form\AnnonceContactType;
 use App\Repository\AnnoncesRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Mailer\MailerInterface;
 
 class AnnoncesController extends AbstractController
 {
     #[Route('/annonces/details/{slug}', name: 'annonce_details')]
-    public function detail($slug, AnnoncesRepository $annoncesRepository): Response
+    public function detail($slug, AnnoncesRepository $annoncesRepository, Request $request, MailerInterface $mailer): Response
     {
         $annonce = $annoncesRepository->findOneby(['slug'=> $slug]);
         if(!$annonce){
             throw new NotFoundHttpException('Pas d\'annonces trouvées');
         }
 
+        $form = $this->createForm(AnnonceContactType::class);
 
-        return $this->render('annonces/details.html.twig', compact('annonce')); 
+        $contact = $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $email = (new TemplatedEmail())
+            ->from($contact->get('email')->getData())
+            ->to($annonce->getUsers()->getEmail())
+            ->subject('contact au sujet de votre annonce')
+            ->htmlTemplate('emails/contact_annonce.html.twig')
+            ->context(['annonce'=> $annonce, "mail" => $contact->get('email')->getData(), 'message' => $contact->get('message')->getData()]);
+            $mailer->send($email);
+            $this->addFlash('message', "votre email a bien été envoyé");
+            
+            return $this->redirectToRoute('annonce_details', ['slug' => $annonce->getSlug()]);
+        }
+
+        return $this->render('annonces/details.html.twig', ["annonce" => $annonce, "form" => $form->createView()] ); 
         
     }
 
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\AnnoncesRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -15,6 +19,31 @@ class MainController extends AbstractController
         $annonce = $annoncesRepository->findby(['active'=>true], ['created_at' => "desc"], 5);
         return $this->render('main/index.html.twig', [
             "annonces" => $annonce
+        ]);
+    }
+
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request, MailerInterface $mailer)
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $contact = $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+         $email = (new TemplatedEmail())
+            ->from($contact->get('email')->getData())
+            ->to("bastienpiperel@gmail.com")
+            ->subject('contact site test')
+            ->htmlTemplate('emails/contact.html.twig')
+            ->context(['mail' => $contact->get('email')->getData(),'sujet' => $contact->get('sujet')->getData(),'message' => $contact->get('message')->getData()]);
+            $mailer->send($email);
+            $this->addFlash('message', 'Mail de contact envoyé');
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('main/contact.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
